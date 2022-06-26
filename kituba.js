a213b1725aa6a662a732de7ebc37c82f58d8d24f
JoshuaDaleRoberts <joshdroberts04@gmail.com>
2022-06-25 22:56:26 -0700

    something's starting to work

--- kituba.js
+++ kituba.js
@@ -10,17 +10,30 @@ const kit = {
     if (a != "ku"){
       return "Verb error"
     }
-    //tenses
     //0 = present
     if (tense == 0){
       finalVerb = verb.substring(2); 
-      
     }
     //1 = present progressive, 
+    if (tense == 1){
+      finalVerb = `ke na ${verb}`
+    }
     //2 = future
+    if (tense == 2){
+      finalVerb = "ke " + verb; 
+    }
     //3 = past 
+    if (tense == 3){
+      finalVerb = verb.substring(2) + "ka"
+    }
     //4 = past participle has 
+   if (tense == 4) {
+    finalVerb = "me " + verb.substring(2); 
+    }
     //5 = past progressive
+    if (tense == 5) {
+      finalVerb = "me " + verb.substring(2) + "ka"; 
+    }
     finalVerb = finalVerb + objMark
     return finalVerb
   }
