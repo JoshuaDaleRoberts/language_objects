2d7df3d762047d639952d1005aaeee6a5a4f0109
JoshuaDaleRoberts <joshdroberts04@gmail.com>
2022-06-25 22:45:15 -0700

    idk what is going on

---
+++ script.js
@@ -0,0 +1,37 @@
+export const kit = {
+  v: function(verb, tense){
+    let finalVerb = ""; let objMark = "";
+    if (verb.includes(" ")) {
+      let spPos = verb.indexOf(" ")
+      objMark = verb.substring(spPos, verb.length);
+      verb = verb.substring(0, spPos);
+    }
+    //tenses
+    //0 = present
+    if (tense == 0){
+      finalVerb = verb.substring(1); 
+    }
+    //1 = present progressive, 
+    //2 = future
+    //3 = past 
+    //4 = past participle has 
+    //5 = past progressive
+    finalVerb = finalVerb + objMark
+    return finalVerb
+  }
+}
+
+
+function testingFunction(){
+  let a =  kit.v("kusingala",0);
+  document.getElementById("console").innerHTML = a;
+}
+
+
+
+
+
+
+
+
+document.body.onload = testingFunction(); 
